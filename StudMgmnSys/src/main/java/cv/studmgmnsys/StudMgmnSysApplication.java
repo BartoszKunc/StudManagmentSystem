7b562dd21185373b5d1609ea84c822d532743b08
BartoszKunc <s24431@pjwstk.edu.pl>
2025-03-07 23:03:17 +0100

    init commit

---
+++ StudMgmnSys/src/main/java/cv/studmgmnsys/StudMgmnSysApplication.java
@@ -0,0 +1,13 @@
+package cv.studmgmnsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudMgmnSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudMgmnSysApplication.class, args);
+	}
+
+}
---
+++ StudMgmnSys/src/test/java/cv/studmgmnsys/StudMgmnSysApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.studmgmnsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudMgmnSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
